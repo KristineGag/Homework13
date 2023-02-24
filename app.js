175e2143db5c9b26af1d37cf219b2166b24bfe0f
KristineGag <christinegagosh@gmail.com>
2023-02-24 13:53:47 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log("Hello, this is my info in js file");
+console.log("Hello, this is my firs info in js file");
 
 const myName = 'Kristine';
 
@@ -18,11 +18,15 @@ let userEmail;
 
 let myEmail = undefined;
 
-console.log(typeof myName, typeof yearOfBirth, typeof iAmNotEmployed, typeof thisWillBeMyNull, typeof userEmail, typeof myEmail);
+console.log('type of myName is', typeof myName,'type of yearOfBirth is', typeof yearOfBirth,'type of iAmNotEmployed is', typeof iAmNotEmployed,'type of thisWillBeMyNull is', typeof thisWillBeMyNull,'type of userEmail is', typeof userEmail,'type of myEmail is also', typeof myEmail);
 
 const myAge = currentYear - yearOfBirth;
 
-console.log('My name is', myName, 'I am', myAge, 'years old and my hobbies are:', myHobby, 'and', myHobby2,);
+console.log('let us try if modulo works')
+let modulo = currentYear % yearOfBirth;
+console.log('you get reminder of', modulo, 'when you devide', currentYear, 'by',  yearOfBirth)
+
+console.log('My name is', myName, 'I am', myAge, 'years old and my hobbies are:', myHobby, 'and', myHobby2);
 
 const textSecond = 'My name is ' + myName + ' I am ' + myAge + ' years old and my hobbies are: ' + myHobby + ' and ' +  myHobby2;
 
